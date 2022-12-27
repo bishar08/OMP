fcc34f841db3d4cf1d79e6602c8411cf19763272
SuleimanZeila <suzedu1@gmail.com>
2022-12-27 14:58:17 +0300

    admin css updated

--- agents_profiles/settings.py
+++ agents_profiles/settings.py
@@ -121,9 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT =Path.joinpath(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
